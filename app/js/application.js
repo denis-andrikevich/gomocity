a31f12cc272e05758f30f19681eb3e8aa1b9f447
Denis Andrikevich <tavtant@yandex.ru>
2017-04-01 13:09:55 +0300

    finished filters

--- app/js/application.js
+++ app/js/application.js
@@ -21,6 +21,25 @@ $(document).ready(function () {
         }
     });
 
+    $("#sliderFilter").slider({
+        min: 0,
+        max: 1000,
+        values: [0, 1000],
+        range: true,
+        stop: function (event, ui) {
+            jQuery("#priceMinFilter").html(jQuery("#sliderFilter").slider("values", 0));
+            jQuery("#priceMaxFilter").html(jQuery("#sliderFilter").slider("values", 1));
+        },
+        slide: function (event, ui) {
+            jQuery("#priceMinFilter").html(jQuery("#sliderFilter").slider("values", 0));
+            jQuery("#priceMaxFilter").html(jQuery("#sliderFilter").slider("values", 1));
+        }
+    });
+
+    $('.list.dropdown').click(function(e){
+        e.stopPropagation();
+    })
+
 
 
     var locations = [
